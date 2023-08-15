1072ab6d44ee727bba43ef0bbbfff67e4ea7c54a
avinashankur <121746164+avinashankur@users.noreply.github.com>
2023-08-16 02:11:39 +0530

    animated bottom menu

--- src/components/MenuBottom/MenuBottom.jsx
+++ src/components/MenuBottom/MenuBottom.jsx
@@ -1,19 +1,66 @@
+"use client";
 import Link from "next/link";
+import { BiHomeAlt2 } from "react-icons/bi";
 import { GoPerson } from "react-icons/go";
 import { LuNewspaper } from "react-icons/lu";
 import { LuBook } from "react-icons/lu";
 import { PiMicrophoneStageBold } from "react-icons/pi";
 import { BsLaptop } from "react-icons/bs";
+import { useState } from "react"; // Import useState hook
 
 const MenuBottom = () => {
+  const [activeLink, setActiveLink] = useState(null); // State to track active link
+
+  // Function to handle link click
+  const handleLinkClick = (index) => {
+    setActiveLink(index);
+  };
+
   return (
     <div id="menu-bottom" className="hidden">
       <div className="flex text-2xl justify-between px-10 py-6">
-        <Link href=""><GoPerson /></Link>
-        <Link href=""><LuNewspaper /></Link>
-        <Link href=""><LuBook /></Link>
-        <Link href=""><PiMicrophoneStageBold /></Link>
-        <Link href=""><BsLaptop /></Link>
+        <Link
+          href="/"
+          className={`menu-link ${activeLink === 0 ? "active" : ""}`}
+          onClick={() => handleLinkClick(0)}
+        >
+          <BiHomeAlt2 />
+        </Link>
+        <Link
+          href="/about"
+          className={`menu-link ${activeLink === 1 ? "active" : ""}`}
+          onClick={() => handleLinkClick(1)}
+        >
+          <GoPerson />
+        </Link>
+        <Link
+          href="/articles"
+          className={`menu-link ${activeLink === 2 ? "active" : ""}`}
+          onClick={() => handleLinkClick(2)}
+        >
+          <LuNewspaper />
+        </Link>
+        <Link
+          href="/projects"
+          className={`menu-link ${activeLink === 3 ? "active" : ""}`}
+          onClick={() => handleLinkClick(3)}
+        >
+          <LuBook />
+        </Link>
+        <Link
+          href="/speaking"
+          className={`menu-link ${activeLink === 4 ? "active" : ""}`}
+          onClick={() => handleLinkClick(4)}
+        >
+          <PiMicrophoneStageBold />
+        </Link>
+        <Link
+          href="/uses"
+          className={`menu-link ${activeLink === 5 ? "active" : ""}`}
+          onClick={() => handleLinkClick(5)}
+        >
+          <BsLaptop />
+        </Link>
       </div>
     </div>
   );
